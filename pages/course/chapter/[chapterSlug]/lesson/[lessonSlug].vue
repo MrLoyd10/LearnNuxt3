c0306134488860aae50a6b51ebf800bb468a6f20
MrLoyd10 <jhonloydocoy@gmail.com>
2024-05-12 16:19:38 +0800

    Getting data base from Slugs

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,12 +1,27 @@
-<script lang="ts" setup></script>
+<script setup>
+  const course = useCourse();   //Auto import na yung useCourse galing sa composables folder./////////
+  const route = useRoute();     //Build in na ito sa nuxt
+
+  const chapter = computed(() => {
+    return course.chapters.find(
+      (chapter) => chapter.slug === route.params.chapterSlug
+    );
+  });
+
+  const lesson = computed(() => {
+    return chapter.value?.lessons.find (
+      (lesson) => lesson.slug === route.params.lessonSlug
+    );
+  });
+</script>
 
 <template>
   <div>
     <h3 class="font-bold mb-2">Lesson</h3>
     <p>This is a lesson</p>
 
-    <p>Chapter Slug: {{ $route.params.chapterSlug }}</p>
-    <p>Lesson Slug: {{ $route.params.lessonSlug }}</p>
+    <p>{{ chapter?.title }}</p>
+    <p>{{ lesson?.title }}</p>
   </div>
 </template>
 
