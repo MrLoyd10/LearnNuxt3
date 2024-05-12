fb816f6535215e1145cccfbbc7cceeafba19489c
MrLoyd10 <jhonloydocoy@gmail.com>
2024-05-12 14:49:42 +0800

    Dynamic routes (using Slugs)

---
+++ pages/course.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+
+    <div class="min-h-screen flex flex-col bg-gray-100 p-12">
+
+      <div class="mb-12">
+        <h1 class="text-2xl text-center">
+          <span class="font-meduim">Course: </span>
+          <span class="font-bold">Mastering Nuxt</span>
+        </h1>
+      </div>
+
+      <div class="grow flex gap-2">
+
+        <div class="w-3/12 bg-white p-4 rounded-md">
+          <h3 class="font-bold">Chapter</h3>
+
+        </div>
+
+        <div class="w-9/12 bg-white p-6 rounded-md">
+          
+          <NuxtPage />
+        
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h3 class="font-bold mb-2">Lesson</h3>
+    <p>This is a lesson</p>
+
+    <p>Chapter Slug: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson Slug: {{ $route.params.lessonSlug }}</p>
+  </div>
+</template>
+
+<style scoped></style>
