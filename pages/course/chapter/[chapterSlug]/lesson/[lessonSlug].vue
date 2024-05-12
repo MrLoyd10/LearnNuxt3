351fbef2c83c391546a83aef368a61caea76523c
MrLoyd10 <jhonloydocoy@gmail.com>
2024-05-12 16:46:46 +0800

    Add styles

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,28 +1,46 @@
 <script setup>
-  const course = useCourse();   //Auto import na yung useCourse galing sa composables folder./////////
-  const route = useRoute();     //Build in na ito sa nuxt
+const course = useCourse(); //Auto import na yung useCourse galing sa composables folder./////////
+const route = useRoute(); //Build in na ito sa nuxt
 
-  const chapter = computed(() => {
-    return course.chapters.find(
-      (chapter) => chapter.slug === route.params.chapterSlug
-    );
-  });
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
 
-  const lesson = computed(() => {
-    return chapter.value?.lessons.find (
-      (lesson) => lesson.slug === route.params.lessonSlug
-    );
-  });
+const lesson = computed(() => {
+  return chapter.value?.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
 </script>
 
 <template>
   <div>
-    <h3 class="font-bold mb-2">Lesson</h3>
-    <p>This is a lesson</p>
+    <h3 class="font-semibold text-slate-400 uppercase mb-2">
+      Lesson {{ chapter?.number }}-{{ lesson?.number }}
+    </h3>
+    
+    <h2 class="text-lg font-semibold upp mb-2">{{ lesson?.title }}</h2>
 
-    <p>{{ chapter?.title }}</p>
-    <p>{{ lesson?.title }}</p>
+    <div class="flex flex-col space-x-4 mb-5">
+      <a v-if="lesson?.sourceUrl"
+      class=" font-normal text-sm underline text-gray-500 hover:text-gray-800"
+      :href="lesson.sourceUrl"> Download URL </a>
+
+      <a v-if="lesson?.downloadUrl"
+      class=" font-normal text-sm underline text-gray-500 hover:text-gray-800" 
+      :href="lesson?.downloadUrl">
+        Download Video
+      </a>
+    </div>
+
+    <p>{{ lesson?.text }}</p>
   </div>
 </template>
 
-<style scoped></style>
+<style>
+* {
+  font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+</style>
